498f43ad368eea5eacecc8dbfe006a336e1c45ba
mwynne777 <Michael@MacBook-Pro-2.local>
2021-01-20 21:14:13 -0600

    Fixing some auth0 issues

--- src/pages/api/callback.ts
+++ src/pages/api/callback.ts
@@ -1,8 +1,19 @@
 import auth0 from '../../utils/auth0';
+import { NextApiRequest } from '../../../node_modules/next';
+
+function patchReqForCallback(req: NextApiRequest): NextApiRequest {
+  const { state } = req.query;
+
+  if (typeof state === 'string') {
+    req.cookies['a0:state'] = state;
+  }
+
+  return req;
+}
 
 export default async function callback(req, res) {
   try {
-    await auth0.handleCallback(req, res, {});
+    await auth0.handleCallback(patchReqForCallback(req), res);
   } catch (error) {
     console.error(error);
     res.status(error.status || 500).end(error.message);
